aece9bb499500606fe0392b20bdbc3a84c67a6fc
jydemagt <michael@sommerlund.net>
2026-08-21 20:01:00 +0200

    waedaw
    add

--- modules/quality/module.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'id'=>'quality','name'=>'Datakvalitet','version'=>'1.0.0','nav'=>true,'href'=>'quality.php','icon'=>'✓',
-    'capability'=>'quality.manage','description'=>'Kvalitetscenter for manglende produktdata, billeder, undtagelser og produktgodkendelse.',
-    'core'=>false,'sort'=>105,
-];
